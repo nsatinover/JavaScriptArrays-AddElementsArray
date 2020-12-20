2e6ca13832107798505502086f065c894cbff024
Nick Satinover <nsatinover.aws@gmail.com>
2020-12-20 12:14:34 -0500

    search-list.js includes indexOf

--- js/playlist-1.js
+++ js/playlist-1.js
@@ -6,3 +6,18 @@ const playlist = [
   'Three Little Birds',
   'The Way You Look Tonight'
 ];
+
+function createListItems(arr) {
+  let items = '';
+  for(let i = 0; i < arr.length; i++){
+    items += `<li>${ arr[i] }</li>`;
+  }
+
+  return items;
+}
+
+document.querySelector('main').innerHTML = `
+  <ol>
+    ${ createListItems(playlist) };
+  </ol>
+`;
--- js/search-list.js
+++ js/search-list.js
@@ -1,3 +1,23 @@
-const inStock = ['pizza', 'cookies', 'eggs', 'apples', 'milk', 'cheese', 'bread', 'lettuce', 'carrots', 'broccoli', 'potatoes', 'crackers', 'onions', 'tofu', 'limes', 'cucumbers'];
+const inStock = ['pizza', 'cookies', 'eggs', 'apples', 'milk', 
+'cheese', 'bread', 'lettuce', 'carrots', 'broccoli', 'potatoes', 
+'crackers', 'onions', 'tofu', 'limes', 'cucumbers'];
+
 const search = prompt('Search for a product.');
+
 let message;
+
+if(!search){
+    message = `<strong>In stock:</strong> ${inStock.join(', ')}`;
+}
+else if(inStock.includes(search.toLowerCase())){
+    message = 
+    `
+    Yes, we have <strong>${search}</strong> in stock. It's
+    #${inStock.indexOf(search.toLowerCase()) + 1} on the list!
+    `;
+}
+else {
+    message = `Sorry, we do not have <strong>${search}</strong> in stock.`
+}
+
+document.querySelector('main').innerHTML = `<p>${message}</p>`;
